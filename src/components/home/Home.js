2b9e49294756d7dd53ca958ae852d33dd4702d51
ZAFER OZCAN <40546836+zaferozzcan@users.noreply.github.com>
2020-08-11 02:33:29 -0400

    Add mapping state to props

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,15 +1,45 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
 
 class Home extends Component {
   render() {
+    // console.log(this.props);
+
+    let ItemList = this.props.items.map((item, value) => (
+      <div className="card" key={item.id}>
+        <div className="card-image">
+          <img src={item.img} alt={item.title} />
+          <span className="card-title">{item.title}</span>
+          <span
+            to="/"
+            className="btn-floating halfway-fab waves-effect waves-light red"
+          >
+            <i className="material-icons">add</i>
+          </span>
+        </div>
+
+        <div className="card-content">
+          <p>{item.desc}</p>
+          <p>
+            <b>Price: {item.price}$</b>
+          </p>
+        </div>
+      </div>
+    ));
+
     return (
       <div className="container">
         <h3 className="center">Our items</h3>
-        <div className="box">
-          <h1> Items List here</h1>
-        </div>
+        <div className="box">{ItemList}</div>
       </div>
     );
   }
 }
-export default Home;
+
+const mapStateToProps = (state) => {
+  return {
+    items: state.items
+  };
+};
+
+export default connect(mapStateToProps)(Home);
